b82cbfdd2f146c33ffc196c02a7b76e0b004812a
Lpslopes <lamartinelps@gmail.com>
2021-05-22 23:02:10 -0300

    Criado atributo complexo filho

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,2 +1,13 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade= idade
+        self.nome = nome
+        self.filhos = list(filhos)
+    def cumprimentar(self):
+        return "Ola"
+if __name__ == '__main__':
+  lamartine = Pessoa(nome="Lamartine",idade=40)
+  luciano = Pessoa(lamartine, nome='luciano')
+  for filho in luciano.filhos:
+      print(filho.nome)
+
