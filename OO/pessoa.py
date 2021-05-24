c399adc7016a9ac44222b66bdabdf233173f2c68
Lpslopes <lamartinelps@gmail.com>
2021-05-24 20:48:29 -0300

    Criado atributo de classe olhos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade= idade
         self.nome = nome
@@ -6,8 +7,14 @@ class Pessoa:
     def cumprimentar(self):
         return "Ola"
 if __name__ == '__main__':
-  lamartine = Pessoa(nome="Lamartine",idade=40)
+  lamartine = Pessoa(nome="Lamartine", idade=40)
   luciano = Pessoa(lamartine, nome='luciano')
+  lamartine.endereço = 'Rua 6'
+  lamartine.olhos = 4
   for filho in luciano.filhos:
       print(filho.nome)
+del lamartine.olhos
+print(lamartine.__dict__)
+print(Pessoa.olhos)
+print(lamartine.olhos)
 
